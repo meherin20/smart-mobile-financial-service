57644fb818221e71e0509fdaa819477efaa31961
meherin sohani <msohani2330420@bscse.uiu.ac.bd>
2026-02-20 19:06:10 +0600

    loginpage

---
+++ script/login.js
@@ -0,0 +1,21 @@
+document.getElementById("login-btn")
+.addEventListener("click",function(){
+      const numberInput=document.getElementById("input-number");
+      const contactNumber=numberInput.value;
+      console.log(contactNumber);
+
+      const inputPin=document.getElementById("input-pin");
+      const pin=inputPin.value;
+      console.log(pin);
+
+      if(contactNumber == "01715545684" && pin == "1234"){
+alert("login successful");
+ window.location.replace=("./home.html");
+      }
+else{
+      alert("Login Failed");
+      return;
+}
+
+
+});
